2fb18247c10f902f4db648c27e9ff5eeedb68bc0
Seldarzu <arzuuxselda@gmail.com>
2025-05-17 20:31:37 +0300

    feat(rbac): Roller ve izinlere dayalı erişim kontrolü eklendi (Admin/User grupları)

--- backend/ai_film/settings.py
+++ backend/ai_film/settings.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "django.contrib.admin",
-    "django.contrib.auth",
-    "django.contrib.contenttypes",
-    "django.contrib.sessions",
-    "django.contrib.messages",
-    "django.contrib.staticfiles",
+   
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+   
+    'core.apps.CoreConfig',      # ← RBAC app
+    'movies',                    # Movie models and serializers
+    'rest_framework',            # DRF for APIs
 ]
 
 MIDDLEWARE = [
---
+++ backend/movies/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
+from .models import Movie
+from .serializers import MovieSerializer
+
+class MovieViewSet(viewsets.ModelViewSet):
+    queryset = Movie.objects.all()
+    serializer_class = MovieSerializer
+    permission_classes = [IsAuthenticated, DjangoModelPermissions]
