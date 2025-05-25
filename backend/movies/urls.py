76fe0340958477a0e9c0234df6984818e0f81797
Alparslan <alparslanb274@gmail.com>
2025-05-25 18:43:01 +0300

    giriş ve kayıt oluşturma endpointleri eklendi

--- backend/movies/urls.py
+++ backend/movies/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from .api import trending_movies, trending_series
+from movies.views import RegisterView, LoginView
 
 urlpatterns = [
     path("trending/movies/", trending_movies, name="trending-movies"),
     path("trending/series/", trending_series, name="trending-series"),
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
 ]
