809975f823bc57c8792e26ca33234344497df80d
Ayaz Aktaş <ayazaktastr@gmail.com>
2025-05-25 14:26:25 +0300

    api for home page Movies and TVshows

---
+++ backend/movies/api.py
@@ -0,0 +1,18 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from models import Movie, TVShow
+from .serializers import MovieSerializer, TVShowSerializer
+
+@api_view(["GET"])
+def trending_movies(request):
+    top_n = int(request.GET.get("limit", 4))
+    qs = Movie.objects.order_by("-popularity")[:top_n]
+    ser = MovieSerializer(qs, many=True, context={"request": request})
+    return Response(ser.data)
+
+@api_view(["GET"])
+def trending_series(request):
+    top_n = int(request.GET.get("limit", 4))
+    qs = TVShow.objects.order_by("-popularity")[:top_n]
+    ser = TVShowSerializer(qs, many=True, context={"request": request})
+    return Response(ser.data)
