c10fb5ae833debedc8861b476eeebd5bb88865e4
Ayaz Aktaş <ayazaktastr@gmail.com>
2025-05-25 10:49:15 +0300

    dockerın ayağa kalkabilmesi için düzenlemeler

--- backend/ai_film/urls.py
+++ backend/ai_film/urls.py
@@ -19,6 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('movies/', include('thirdPartyLib.urls', namespace='movies')),
     path('api/', include('recommendation.urls')),
 ]
