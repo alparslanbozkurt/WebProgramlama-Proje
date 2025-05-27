b944855a0d139b57326f2c49c7978b0903c7a4ba
Ayaz Aktaş <ayazaktastr@gmail.com>
2025-05-27 20:53:12 +0300

    Filmlerin genreye göre filtrelenmesi

--- backend/movies/views.py
+++ backend/movies/views.py
@@ -1,12 +1,13 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from .models import Movie, TVShow
+from .models import Movie, TVShow, Genre
 from .serializers import (
     MovieSerializer,
     MovieDetailSerializer,
     TVShowSerializer,
     TVShowDetailSerializer,
+    GenreSerializer
 )
 
 class MovieViewSet(viewsets.ModelViewSet):
@@ -37,3 +38,8 @@ class TVShowViewSet(viewsets.ModelViewSet):
         top8 = self.get_queryset()[:200]
         ser = self.get_serializer(top8, many=True)
         return Response(ser.data)
+
+
+class GenreViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = Genre.objects.all().order_by('name')
+    serializer_class = GenreSerializer
